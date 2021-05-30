653b1c982ef5f0db62e8b1a814d320d7280fa912
UNISUPER\rineshk <rinesh.kumar@unisuper.com.au>
2021-05-30 11:29:45 +1000

    Arrays

--- src/0091_ArrayUtilities.ts
+++ src/0091_ArrayUtilities.ts
@@ -1,2 +1,23 @@
 const numberArray : number[] = [1,2,3,4,5];
-console.log(numberArray)
+console.log(numberArray) // [1,2,3,4,5]
+numberArray.push(10);
+console.log(numberArray) // [1,2,3,4,5,10]
+numberArray.pop();
+console.log(numberArray) // [1,2,3,4,5]
+let slicedArray  = numberArray.slice(0,3);
+console.log(slicedArray) // [1,2,3]
+console.log(numberArray) // [1,2,3,4,5]
+let splicedArray = numberArray.splice(0,3);
+console.log(splicedArray) // [1,2,3]
+console.log(numberArray) // [4,5]
+
+
+let unsortedArray : number[] = [5,4,1,2,3];
+console.log(unsortedArray.sort());// [1,2,3,4,5]
+
+console.log(unsortedArray.join());// 1,2,3,4,5 -- String 
+
+console.log(unsortedArray.includes(5)); // true 
+
+console.log(numberArray.concat(unsortedArray).sort());//[1, 2, 3, 4,4, 5, 5]
+
